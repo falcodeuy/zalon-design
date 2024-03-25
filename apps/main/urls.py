6b66d4e3f8c425d49477dba2f3b92c061357239b
Fausto Márquez <fausto_7m@hotmail.com>
2024-03-24 23:36:46 -0300

    WIP Refactor home page and add pack detail view

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -4,4 +4,10 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.home, name="home"),
+    path(
+        "pack/<slug:slug>/",
+        views.pack_detail,
+        name="pack_detail",
+        kwargs={"slug": "slug"},
+    ),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from .models import Pack
+
 
 def home(request):
-    return render(request, "main/home.html", {})
+    packs = Pack.objects.filter(is_active=True, show_in_landing=True)
+    context = {"packs": packs}
+
+    return render(request, "main/home.html", context)
+
+
+def pack_detail(request, slug):
+    pack = Pack.objects.get(slug=slug)
+    context = {"pack": pack}
+
+    return render(request, "main/pack_detail.html", context)
