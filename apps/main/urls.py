bbc53dd3ab845294e7513ef05b732a417852bf54
Fausto Márquez <fausto_7m@hotmail.com>
2024-04-01 18:05:33 -0300

    Update style.css and urls.py, add pack_purchase view and pack_purchase_form.html

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.home, name="home"),
-    path(
-        "pack/<int:id>/",
-        views.pack_detail,
-        name="pack_detail",
-    ),
+    path("pack/<int:id>/", views.pack_detail, name="pack_detail"),
+    path("pack/purchase/<int:id>", views.pack_purchase, name="pack_purchase"),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -7,12 +7,16 @@ def home(request):
     packs = Pack.objects.filter(is_active=True, show_in_landing=True)
     customer_reviews = CustomerReview.objects.all()[:25]
     context = {"packs": packs, "customer_reviews": customer_reviews}
-
     return render(request, "main/home.html", context)
 
 
 def pack_detail(request, id):
     pack = Pack.objects.get(id=id)
     context = {"pack": pack}
-
     return render(request, "main/pack_detail.html", context)
+
+
+def pack_purchase(request, id):
+    pack = Pack.objects.get(id=id)
+    context = {"pack": pack}
+    return render(request, "main/pack_purchase_form.html", context)
