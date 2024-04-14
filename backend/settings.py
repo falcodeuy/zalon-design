f5f677f45b49960eef0f92b2cab99f650a81a7b4
Fausto Márquez <fausto_7m@hotmail.com>
2024-04-14 14:51:11 -0300

    Update database configuration

--- backend/settings.py
+++ backend/settings.py
@@ -89,6 +89,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
